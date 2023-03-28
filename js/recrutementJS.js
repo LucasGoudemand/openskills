4815e85d9c07f46e0d86acac2e3da0cc17bd3a8f
Lucas Goudemand <goudemand.lucas@gmail.com>
2023-03-28 09:54:44 +0200

    update page recrutement

--- js/recrutementJS.js
+++ js/recrutementJS.js
@@ -16,21 +16,3 @@ window.addEventListener("scroll", () => {
     slidingBubbleRecrutementRightContainer.classList.add("active");
   }
 });
-
-const slidingBubbleRecrutementb6Container = document.querySelector(
-  ".text-content-b6-bubble"
-);
-
-window.addEventListener("scroll", () => {
-  const { scrollTop, clientHeight } = document.documentElement;
-
-  const elementToTopViewport =
-    slidingBubbleRecrutementb6Container.getBoundingClientRect().top;
-
-  if (
-    scrollTop >
-    (scrollTop + elementToTopViewport).toFixed() - clientHeight * 0.5
-  ) {
-    slidingBubbleRecrutementb6Container.classList.add("active");
-  }
-});
